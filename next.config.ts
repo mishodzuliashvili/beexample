706cc0654e3c0accc0a15423c79beccfbf551113
Misho Dzuliashvili <mishodzuliashvili1@gmail.com>
2025-03-29 15:17:46 +0400

    fix photo size upload limit

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [{ hostname: "**", pathname: "**", protocol: "https" }],
     },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "10mb",
+        },
+    },
 };
 
 export default nextConfig;
