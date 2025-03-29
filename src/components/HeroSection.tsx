0b5f514580ce3d4cdd1e25d05536bcdce3450e6e
lukabatoni <150857339+lukabatoni@users.noreply.github.com>
2025-03-29 15:46:44 +0400

    test hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -84,7 +84,7 @@ const BeExampleLanding = () => {
                     </div>
 
                     <h1 className="text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl mb-8">
-                        HI Quick check-ins for{" "}
+                        I am from Luka. HI Quick check-ins for{" "}
                         <span className="text-blue-600">busy people</span>
                     </h1>
 
